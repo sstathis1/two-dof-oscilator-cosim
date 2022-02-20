07f404fb2c7eff9ada8d4b49b748cfd6de54ee55
StevennStathis <stefanosdims@gmail.com>
2022-02-20 15:21:35 +0200

    Changed the comments

--- cs_test.py
+++ cs_test.py
@@ -1,7 +1,10 @@
 """
-Created on Tue Nov  2 17:58:39 2021
+Created on Tue Nov 2 17:58:39 2021
 
-@author: MrStevenn007
+@author: Stefanos Stathis
+
+Contains a test script that executes an explicit co-simulation of a linear 2dof oscilator and plots the position response, 
+as well as the global and local errors.
 """
 
 from Richardson import Orchestrator
@@ -10,30 +13,42 @@ import time
 
 start_time = time.perf_counter()
 
-# Import Data of 2-DOF Linear Oscilator
-m = 10 # (kg)
-k = 10 # (N/m)
+# Specify the data of the 2-DOF linear oscilator
+m = 10   # (kg)
+k = 10   # (N/m)
 c = 10   # (Nsec/m)
 cc = 5   # (Nsec/m)
 
-# Import Initial Conditions for the Simulation
+# Specify the initial conditions for the simulation
 x10 = 0  # (m)
 x20 = 0  # (m)
-v10 = 1 # (m/sec)
+v10 = 1  # (m/sec)
 v20 = 0  # (m/sec)
-lc0 = k*(x20 - x10) + cc*(v20 - v10) # (N) Coupling Force at t = 0
+
+# Coupling Force at t = 0 (N)
+lc0 = k*(x20 - x10) + cc*(v20 - v10)
+
 initial1 = np.array([[x10], [v10]])
 initial2 = np.array([[x20], [v20]])
 
-# Set Co-Simulation parameters
-t0 = 0 # Initial Time of Simulation
-tf = 12 # Final Time of Simulation
-H = 1e-2 # Macro-Step of Co-Sim
-polyDegree = 0 # Polynomial Interpolation degree
-h = 1 # Micro-Step of Numerical Integration
-Model1Method = 'Force' # Oscilation Method of Model 1   
-Model2Method = 'Force' # Oscilation Method of Model 2
-CoSimMethod = 'Gauss' # Co-Simulation Method for coupling variables
+# Final time
+tf = 12
+
+# Macro step
+H = 1e-2
+
+# Interpolation / Extrapolation degree
+polyDegree = 0
+
+# Micro step
+h = 1
+
+# Oscilation method of models
+Model1Method = 'Force'
+Model2Method = 'Force'
+
+# Co-simulation comunication method to use
+CoSimMethod = 'Gauss'
 
 if Model1Method == 'Force':
     y2 = lc0
@@ -49,15 +64,14 @@ else:
 Co_Sim = Orchestrator(H, polyDegree, tf, k, cc, CoSimMethod)
 
 # Create the 2 Subsystem models
-Co_Sim.setModel1(m, k, c, Model1Method, 'RK45', h) # First Subsystem
-Co_Sim.setModel2(m, k, c, Model2Method, 'RK45', h) # Second Subsystem
+Co_Sim.setModel1(m, k, c, Model1Method, 'RK45', h)
+Co_Sim.setModel2(m, k, c, Model2Method, 'RK45', h)
 
 # Begin the Co-Simulation
 Co_Sim.beginSimulation(initial1, initial2, y1, y2)
 
 end_time = time.perf_counter()
-print(f"Process finished in : {end_time-start_time} second(s)")
+print(f"Co-Simulation finished correctly in : {end_time-start_time} second(s)")
 
 Co_Sim.plotOutputs()
 Co_Sim.plotLocalError()
-Co_Sim.plotStepSize()
