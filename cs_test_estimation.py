3a7500dbda1f4dd89b9962c10c423b6787fc609e
StevennStathis <stefanosdims@gmail.com>
2022-03-19 22:05:35 +0200

    Finilised script

--- cs_test_estimation.py
+++ cs_test_estimation.py
@@ -1,127 +1,138 @@
 """
-Created on Tue Nov  2 17:58:39 2021
+Contains a test script for ploting a compare between the local error due to co-simulation
+and the error estimated through richardson extrapolation technique.
 
-@author: MrStevenn007
+@author: Stefanos Stathis
 """
 
-from Orchestrator_2DoF_Richardson_1 import Orchestrator as OrchestratorRichardson
-from Orchestrator_2DoF_local_error import Orchestrator
-import numpy as np
+from Richardson import Orchestrator as master_richardson
+from Local_Error import Orchestrator as master_local
 import matplotlib.pyplot as plt
+import numpy as np
+import time
 
-# Import Data of 2-DOF Linear Oscilator
-m = 10 # (kg)
-k = 10 # (N/m)
-c = 10   # (Nsec/m)
-cc = 5   # (Nsec/m)
+start_time = time.perf_counter()
 
-# Import Initial Conditions for the Simulation
+# Specify the data of the 2-DOF linear oscilator
+m = 1    # (kg)
+k = 100  # (N/m)
+
+# Specify a Rayleigh damping. C = β * Κ where β = c / k = 1 / (10 * ω1)
+c = 0.1 * (k * m)**0.5   # (Nsec/m)
+cc = c   # (Nsec/m)
+
+# Specify the initial conditions for the simulation
 x10 = 0  # (m)
-x20 = 0  # (m)
-v10 = 1 # (m/sec)
+x20 = 1  # (m)
+v10 = 1  # (m/sec)
 v20 = 0  # (m/sec)
-lc0 = k*(x20 - x10) + cc*(v20 - v10) # (N) Coupling Force at t = 0
 
-# Initial Condition vectors
+# Coupling Force at t = 0 (N)
+lc0 = k*(x20 - x10) + cc*(v20 - v10)
+
 initial1 = np.array([[x10], [v10]])
 initial2 = np.array([[x20], [v20]])
 
-# Set Co-Simulation parameters
-t0 = 0 # Initial Time of Simulation
-tf = 12 # Final Time of Simulation
-H = 1e-2 # Macro-Step of Co-Sim
-polyDegree = 1 # Polynomial Interpolation degree
-h = 1 # Micro-Step of Numerical Integration
-Model1Method = 'Disp' # Oscilation Method of Model 1   
-Model2Method = 'Disp' # Oscilation Method of Model 2
-CoSimMethod = 'Gauss' # Co-Simulation Method for coupling variables
+# Final time
+tf = 12
 
-if Model1Method == 'Force':
+# Interpolation / Extrapolation degree
+polyDegree = 0
+
+# Macro step
+H = 1e-2
+
+micro_steps = 5
+
+# Oscilation method of models
+Model1Method = "Disp"
+Model2Method = "Disp"
+
+# Solver to use
+solver_first = "Newmark"
+solver_second = "RK45"
+
+# Co-simulation comunication method to use
+CoSimMethod = "Jacobi"
+
+if Model1Method == "Force":
     y2 = lc0
 else:
     y2 = initial2
 
-if Model2Method == 'Force':
+if Model2Method == "Force":
     y1 = -lc0
 else:
     y1 = initial1
 
 # Initialize the Co-Simulation for the analytical local error
-Co_Sim = Orchestrator(H, polyDegree, tf, k, cc, CoSimMethod)
+print("Begining of simulation for local error...")
+Co_Sim = master_local(H, polyDegree, tf, k, cc, CoSimMethod)
 
 # Create the 2 Subsystem models
-Co_Sim.setModel1(m, k, c, Model1Method, 'RK45', h) # First Subsystem
-Co_Sim.setModel2(m, k, c, Model2Method, 'RK45', h) # Second Subsystem
+Co_Sim.setModel1(m, k, c, Model1Method, solver_first, micro_steps) # First Subsystem
+Co_Sim.setModel2(m, k, c, Model2Method, solver_second, micro_steps) # Second Subsystem
 
 # Begin the Co-Simulation for the analytical local error
+sim_start_local = time.perf_counter()
 (localErrorX1, localErrorX2, localErrorV1,
- localErrorV2, localErrorY1, localErrorY2) = Co_Sim.beginSimulation(initial1, initial2, y1, y2)
- 
-print("Finished local error simulation.\n")
+localErrorV2, localErrorY1, localErrorY2) = Co_Sim.beginSimulation(initial1, initial2, y1, y2)
+sim_end_local = time.perf_counter()
+print(f"Succesfully finished local error simulation in {sim_end_local - sim_start_local} second(s)")
 
 # Initialize the Co-Simulation for the Richardson Extrapolation Error Estimate
-Co_SimRichardson = OrchestratorRichardson(H, polyDegree, tf, k, cc, CoSimMethod)
+print("Begining of simulation for richardson estimation...")
+Co_SimRichardson = master_richardson(H, polyDegree, tf, k, cc, CoSimMethod)
 
 # Create the 2 Subsystem models
-Co_SimRichardson.setModel1(m, k, c, Model1Method, 'RK45', h) # First Subsystem
-Co_SimRichardson.setModel2(m, k, c, Model2Method, 'RK45', h) # Second Subsystem
+Co_SimRichardson.setModel1(m, k, c, Model1Method, solver_first, micro_steps) # First Subsystem
+Co_SimRichardson.setModel2(m, k, c, Model2Method, solver_second, micro_steps) # Second Subsystem
 
-# # Begin the Co-Simulation for the richardson extrapolation local error
-(ERichY1, ERichY2) = Co_SimRichardson.beginSimulation(initial1, initial2, y1, y2)
+# Begin the Co-Simulation for the richardson extrapolation local error
+sim_start_richardson = time.perf_counter()
+ERichY1, ERichY2 = Co_SimRichardson.beginSimulation(initial1, initial2, y1, y2)
+sim_end_richardson = time.perf_counter()
+print(f"Succesfully finished richardson estimation simulation in {sim_end_richardson - sim_start_richardson} second(s)")
 
-print("Finished Richardson extrapolation simulation.\n")
+end_time = time.perf_counter()
+print(f"Succesfully finished simulations in {end_time - start_time} second(s)")
 
 # Plot Local Error of outputs
-if polyDegree == 0:
-    start_time = 0
-else:
-    start_time = 10
-
 plt.figure(figsize=(14,8))
 plt.title(f'Τοπικό Σφαλμα εξόδων λόγω συν-προσομοίωσης: {CoSimMethod}, {Model1Method} - {Model2Method}, k = {polyDegree}')
-plt.plot(Co_Sim.time[start_time:], localErrorY1[0, start_time:], label='Local Error $y_1$')
-plt.plot(Co_Sim.time[start_time:], localErrorY2[0, start_time:], label='Local Error $y_2$')
-plt.plot(Co_Sim.time[start_time:], ERichY1[0, start_time:], '--', 
+plt.plot(Co_Sim.time[10:], localErrorY1[0, 10:], label='Local Error $y_1$')
+plt.plot(Co_Sim.time[10:], localErrorY2[0, 10:], label='Local Error $y_2$')
+plt.plot(Co_Sim.time[10:], ERichY1[0, 10:], '--', 
           label='Richardson Extrapolation Estimation $Y_1$')
-plt.plot(Co_Sim.time[start_time:], ERichY2[0, start_time:], '--',
+plt.plot(Co_Sim.time[10:], ERichY2[0, 10:], '--',
           label='Richardson Extrapolation Estimation $Y_2$')
-plt.xlabel('time (sec)')
+plt.xlabel('time (s)')
 plt.ylabel('Local Error y')
 plt.grid()
 plt.xlim(xmin=0, xmax=tf)
 plt.legend()
-plt.show()   
+plt.show()
 
-print("FINISHED SIMULATION\n")
+# Plot Local Error of position
+plt.figure(figsize=(14,8))
+plt.title('Τοπικό Σφαλμα θέσεων λόγω συν-προσομοίωσης')
+plt.plot(Co_Sim.time[10:], localErrorX1[10:, 0], label='Local Error $x_1$')
+plt.plot(Co_Sim.time[10:], localErrorX2[10:, 0], label='Local Error $x_2$')
+plt.xlabel('time (s)')
+plt.ylabel('Local Error x (m)')
+plt.grid()
+plt.xlim(xmin=0, xmax=tf)
+plt.legend()
+plt.show() 
 
-# # Plot Local Error of position
-# plt.figure(figsize=(14,8))
-# plt.title('Τοπικό Σφαλμα θέσεων λόγω συν-προσομοίωσης')
-# plt.plot(Co_Sim.time, localErrorX1[:, 0], label='Local Error $x_1$')
-# plt.plot(Co_Sim.time, localErrorX2[:, 0], label='Local Error $x_2$')
-# plt.plot(Co_Sim.time, ERichX1[0, :], '--', 
-#           label='Richardson Extrapolation Estimation $x_1$')
-# plt.plot(Co_Sim.time, ERichX2[0, :], '--',
-#           label='Richardson Extrapolation Estimation $x_2$')
-# plt.xlabel('time (sec)')
-# plt.ylabel('Local Error x (m)')
-# plt.grid()
-# plt.xlim(xmin=0, xmax=tf)
-# plt.legend()
-# plt.show() 
-
-# # Plot Local Error of velocities
-# plt.figure(figsize=(14,8))
-# plt.title('Τοπικό Σφαλμα ταχυτήτων λόγω συν-προσομοίωσης')
-# plt.plot(Co_Sim.time, localErrorV1[:, 0], label='Local Error $v_1$')
-# plt.plot(Co_Sim.time, localErrorV2[:, 0], label='Local Error $v_2$')
-# plt.plot(Co_Sim.time, ERichV1[0, :], '--', 
-#           label='Richardson Extrapolation Estimation $v_1$')
-# plt.plot(Co_Sim.time, ERichV2[0, :], '--',
-#           label='Richardson Extrapolation Estimation $v_2$')
-# plt.xlabel('time (sec)')
-# plt.ylabel('Local Error v (m/sec)')
-# plt.grid()
-# plt.xlim(xmin=0, xmax=tf)
-# plt.legend()
-# plt.show()  
+# Plot Local Error of velocities
+plt.figure(figsize=(14,8))
+plt.title('Τοπικό Σφαλμα ταχυτήτων λόγω συν-προσομοίωσης')
+plt.plot(Co_Sim.time[10:], localErrorV1[10:, 0], label='Local Error $v_1$')
+plt.plot(Co_Sim.time[10:], localErrorV2[10:, 0], label='Local Error $v_2$')
+plt.xlabel('time (s)')
+plt.ylabel('Local Error v (m/s)')
+plt.grid()
+plt.xlim(xmin=0, xmax=tf)
+plt.legend()
+plt.show()  
