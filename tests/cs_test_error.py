225b697a9f11c096856003d17efd8414a2f96aa4
sstathis1 <stefanosdims@gmail.com>
2022-04-24 12:15:09 +0300

    Changed ploting

--- tests/cs_test_error.py
+++ tests/cs_test_error.py
@@ -50,7 +50,7 @@ solver_first = "Newmark"
 solver_second = "RK45"
 
 # Co-simulation comunication method to use
-CoSimMethod = "Gauss"
+CoSimMethod = "Jacobi"
 
 if Model1Method == "Force":
     y2 = lc0
@@ -66,50 +66,82 @@ else:
 h = np.array([1e-2, 0.5e-2, 1e-3])
 
 # Initialization of error arrays
-errorX1 = np.zeros([len(h), 1])
-errorV1 = np.zeros([len(h), 1])
-errorX2 = np.zeros([len(h), 1])
-errorV2 = np.zeros([len(h), 1])
+errorX1 = np.zeros([3*len(h), 1])
+errorV1 = np.zeros([3*len(h), 1])
+errorX2 = np.zeros([3*len(h), 1])
+errorV2 = np.zeros([3*len(h), 1])
 
 # Loop through macro step list
+j = 0
 for i in range(len(h)):
-    Co_Sim = Orchestrator(h[i], polyDegree, tf, k, cc, CoSimMethod)
-    Co_Sim.setModel1(m, k, c, Model1Method, solver_first, micro_steps) # First Subsystem
-    Co_Sim.setModel2(m, k, c, Model2Method, solver_second, micro_steps) # Second Subsystem
-    print(f"Begining simulation with H = {h[i]}...")
-    sim_start = time.perf_counter()
-    absoluteError1, absoluteError2 = Co_Sim.beginSimulation(initial1, initial2, y1, y2)
-    sim_finish = time.perf_counter()
-    print(f"Finished simulation of H = {h[i]} in {sim_finish - sim_start} second(s)")
-    rmsErrorX1 = np.sqrt(np.mean(absoluteError1[0, 10::]**2))
-    rmsErrorX2 = np.sqrt(np.mean(absoluteError2[0, 10::]**2))
-    rmsErrorV1 = np.sqrt(np.mean(absoluteError1[1, 10::]**2))
-    rmsErrorV2 = np.sqrt(np.mean(absoluteError2[1, 10::]**2))
-    errorX1[i, 0] = rmsErrorX1 
-    errorX2[i, 0] = rmsErrorX2
-    errorV1[i, 0] = rmsErrorV1
-    errorV2[i, 0] = rmsErrorV2
-    print(f"Succesfully saved rms errors, moving to next simulation...")
+    for polyDegree in [0, 1, 2]:
+        Co_Sim = Orchestrator(h[i], polyDegree, tf, k, cc, CoSimMethod)
+        Co_Sim.setModel1(m, k, c, Model1Method, solver_first, micro_steps) # First Subsystem
+        Co_Sim.setModel2(m, k, c, Model2Method, solver_second, micro_steps) # Second Subsystem
+        print(f"Begining simulation with H = {h[i]}...")
+        sim_start = time.perf_counter()
+        absoluteError1, absoluteError2 = Co_Sim.beginSimulation(initial1, initial2, y1, y2)
+        sim_finish = time.perf_counter()
+        print(f"Finished simulation of H = {h[i]} in {sim_finish - sim_start} second(s)")
+        rmsErrorX1 = np.sqrt(np.mean(absoluteError1[0, 30::]**2))
+        rmsErrorX2 = np.sqrt(np.mean(absoluteError2[0, 30::]**2))
+        rmsErrorV1 = np.sqrt(np.mean(absoluteError1[1, 30::]**2))
+        rmsErrorV2 = np.sqrt(np.mean(absoluteError2[1, 30::]**2))
+        errorX1[j, 0] = rmsErrorX1 
+        errorX2[j, 0] = rmsErrorX2
+        errorV1[j, 0] = rmsErrorV1
+        errorV2[j, 0] = rmsErrorV2
+        j += 1
+        print(f"Succesfully saved rms errors, moving to next simulation...")
+        print()
    
-y1 = h**(polyDegree + 1)
-y2 = h**(polyDegree + 2)
+y1 = h**(1)
+y2 = h**(2)
+y3 = h**(3)
 
 end_time = time.perf_counter()
 print(f"Co-Simulations finished correctly in : {end_time-start_time} second(s)")
 
-# Plot error convergence orders
-plt.figure(figsize=(14,8))
-plt.title(f"Rms Global Error - H, {CoSimMethod}, {Model1Method} - {Model2Method}, polydegree = {polyDegree}")
-plt.plot(h, errorX1[:, 0], '*-', label='Τοπικό Σφάλμα Θέσεων - $x_1$')
-plt.plot(h, errorV1[:, 0], '*-', label='Τοπικό Σφάλμα Ταχυτήτων - $v_1$')
-plt.plot(h, errorX2[:, 0], '*-', label='Τοπικό Σφάλμα Θέσεων - $x_2$')
-plt.plot(h, errorV2[:, 0], '*-', label='Τοπικό Σφάλμα Ταχυτήτων - $v_2$')
-plt.plot(h, y1, '--', label='$Ο(H^{k+1})$')
-plt.plot(h, y2, '--', label='$Ο(H^{k+2})$')
-plt.xlabel("Macro Step Size - h (log)")
-plt.ylabel("Global Error $e_{rms}$ (log)")
-plt.grid()
-plt.yscale("log")
-plt.xscale("log")
-plt.legend()
+# Plot global error convergence orders
+SMALL_SIZE = 9
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+plt.rc('font', size=SMALL_SIZE, weight = 'bold')          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE, labelweight = 'bold')     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE, labelweight = 'bold')    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+fig, axs = plt.subplots(1, 2, dpi=150)
+fig.suptitle(f"Τάξη ολικών σφαλμάτων μέσω: {CoSimMethod}, {Model1Method} - {Model2Method}", fontweight='bold')
+
+# Position subplots
+axs[0].set_title("θέσεων $x_1$", fontweight='bold')
+axs[0].plot(h, errorX1[0::3, 0], '*-', linewidth=3.0)
+axs[0].plot(h, errorX1[1::3, 0], '*-', linewidth=3.0)
+axs[0].plot(h, errorX1[2::3, 0], '*-', linewidth=3.0)
+axs[0].plot(h, y1, '--', linewidth=3.0)
+axs[0].plot(h, y2, '--', linewidth=3.0)
+axs[0].plot(h, y3, '--', linewidth=3.0)
+axs[0].set(xlabel = "Βήμα επικοινωνίας H (log)", ylabel="Ολικό σφάλμα $e^x_{rms}$ (log)")
+axs[0].grid()
+axs[0].set_xscale("log")
+axs[0].set_yscale("log")
+
+# Velocity subplots
+axs[1].set_title(f"ταχυτήτων $v_1$", fontweight='bold')
+axs[1].plot(h, errorV1[0::3, 0], '*-', label="k=0", linewidth=3.0)
+axs[1].plot(h, errorV1[1::3, 0], '*-', label="k=1", linewidth=3.0)
+axs[1].plot(h, errorV1[2::3, 0], '*-', label="k=2", linewidth=3.0)
+axs[1].plot(h, y1, '--', label="$O(H^1)$", linewidth=3.0)
+axs[1].plot(h, y2, '--', label="$O(H^2)$", linewidth=3.0)
+axs[1].plot(h, y3, '--', label="$O(H^3)$", linewidth=3.0)
+axs[1].set(xlabel = "Βήμα επικοινωνίας H (log)", ylabel="Ολικό σφάλμα $e^v_{rms}$ (log)")
+axs[1].grid()
+axs[1].set_yscale("log")
+axs[1].set_xscale("log")
+plt.legend(bbox_to_anchor=(1,1), loc="upper left")
 plt.show()
